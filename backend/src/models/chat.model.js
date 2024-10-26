1dfef487126ca5c0ca88a539a6a804070203bf3c
krishan <krishanmondal273@gmail.com>
2024-10-26 18:25:00 +0530

    Create chat model

---
+++ backend/src/models/chat.model.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+
+const chatSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    isGroupChat: {
+      type: Boolean,
+      required: true,
+    },
+    lastMessage: {
+      type: mongoose.Types.ObjectId(),
+      ref: "Message",
+    },
+    participants: [
+      {
+        type: mongoose.Types.ObjectId(),
+        ref: "User",
+      },
+    ],
+    admin: {
+      type: mongoose.Types.ObjectId(),
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Chat = mongoose.model("Chat", chatSchema);
