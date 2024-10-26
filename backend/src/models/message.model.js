f3c7d8246ea63dd60e0dffefcf04b7cbaa2c5267
krishan <krishanmondal273@gmail.com>
2024-10-26 18:33:37 +0530

    Create message model

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+const messageSchema = new Schema(
+  {
+    sender: {
+      type: mongoose.Types.ObjectId(),
+      ref: "User",
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    attachments: {
+      type: [String],
+      default: [],
+    },
+    chat: {
+      type: mongoose.Types.ObjectId(),
+      ref: "Chat",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Message = mongoose.model("Message", messageSchema);
